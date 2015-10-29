6989f863f146f4dd9da9d8ae83de2f70647f7dc4
Peter Chapman <pchapman@easystreet.net>
2015-10-29 23:36:53 +0100

    Adding project files

---
+++ core/src/main/java/com/chapman/service/impl/RoleManagerImpl.java
@@ -0,0 +1,53 @@
+package com.chapman.service.impl;
+
+import com.chapman.dao.RoleDao;
+import com.chapman.model.Role;
+import com.chapman.service.RoleManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Implementation of RoleManager interface.
+ *
+ * @author <a href="mailto:dan@getrolling.com">Dan Kibler</a>
+ */
+@Service("roleManager")
+public class RoleManagerImpl extends GenericManagerImpl<Role, Long> implements RoleManager {
+    RoleDao roleDao;
+
+    @Autowired
+    public RoleManagerImpl(RoleDao roleDao) {
+        super(roleDao);
+        this.roleDao = roleDao;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<Role> getRoles(Role role) {
+        return dao.getAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Role getRole(String rolename) {
+        return roleDao.getRoleByName(rolename);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Role saveRole(Role role) {
+        return dao.save(role);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void removeRole(String rolename) {
+        roleDao.removeRole(rolename);
+    }
+}
